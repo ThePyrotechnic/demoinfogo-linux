234e789330076c3024842a66db9ae19f826ef35d
bugdone <bugdone@gmail.com>
2015-12-06 15:32:20 +0200

    player_hurt event

--- src/demofiledump.cpp
+++ src/demofiledump.cpp
@@ -113,6 +113,7 @@ static std::set<std::wstring> hsbox_events = {L"player_death",
                                               L"player_spawn",
                                               L"game_restart",
                                               L"score_changed",
+                                              L"player_hurt",
                                               L"round_officially_ended"};
 
 void addEvent(const std::map<std::wstring, json_spirit::wmConfig::Value_type> &object_) {
